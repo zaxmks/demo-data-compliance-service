4f3a6c630e957f9bff4a80106707e2d87e1b2587
Jonathan Mugan <jonathanwilliammugan@gmail.com>
2021-03-28 22:45:53 -0500

    File cleanup

--- src/server.py
+++ src/server.py
@@ -5,4 +5,4 @@ from src.web.app.factory import create_app
 main_app = create_app()
 
 if __name__ in "__main__":
-    uvicorn.run(main_app, host="0.0.0.0", port=8081)
+    uvicorn.run(main_app, host="0.0.0.0", port=8080)
