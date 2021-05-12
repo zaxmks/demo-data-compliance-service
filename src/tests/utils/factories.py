fb43327cb57943534edc333500de41aebcdcd991
Michael Wharton <michael@kungfu.ai>
2021-05-12 16:27:15 -0500

    Add commit arg

--- src/tests/utils/factories.py
+++ src/tests/utils/factories.py
@@ -12,50 +12,66 @@ from src.core.db.models.pdf_models import (
     ParsingStrategyType,
 )
 
-main_session = MainDbSession().instance
-pdf_session = PdfDbSession().instance
+main_session = MainDbSession()._session_instance
+pdf_session = PdfDbSession()._session_instance
+
 
 class MainDocumentTypeFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = DocumentType
         sqlalchemy_session = main_session
-    id = fuzzy.FuzzyText(length=32)
+        sqlalchemy_session_persistence = "commit"
+
+    id = factory.Faker("numerify", text="#" * 32)
     name = fuzzy.FuzzyChoice(["UNKNOWN", "FINCEN8300"])
+
 
 class MainEmployeeFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = Employee
         sqlalchemy_session = main_session
+        sqlalchemy_session_persistence = "commit"
+
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
     ssn = factory.Faker("numerify", text="#########")
     date_of_birth = factory.Faker("date", pattern="%m/%d/%Y")
 
+
 class PDFIngestionEventFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = IngestionEvent
         sqlalchemy_session = pdf_session
+        sqlalchemy_session_persistence = "commit"
+
     id = fuzzy.FuzzyChoice(["ddb8d772-c0a4-42ac-9bff-fe4409495988"])
     s3_bucket = factory.Faker("text")
     s3_key = factory.Faker("text")
     created_at = factory.Faker("date", pattern="%Y-%m-%d %H:%M:%S.%f")
     updated_at = factory.Faker("date", pattern="%Y-%m-%d %H:%M:%S.%f")
-    parsing_strategy_type_id = fuzzy.FuzzyChoice(["9720cb1c-4461-40e4-b800-38dfdfd0061b"])
+    parsing_strategy_type_id = fuzzy.FuzzyChoice(
+        ["9720cb1c-4461-40e4-b800-38dfdfd0061b"]
+    )
 
 
 class PDFUnstructuredDocumentFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = UnstructuredDocument
         sqlalchemy_session = pdf_session
+        sqlalchemy_session_persistence = "commit"
+
     id = fuzzy.FuzzyChoice(["de33fee8-ec15-4829-8d0d-fc7cf4206429"])
     name = fuzzy.FuzzyChoice([json.dumps(["Jacqueline", "Baranov"])])
     ssn = fuzzy.FuzzyChoice([json.dumps([761870877])])
     text = factory.Faker("text")
     ingestion_event_id = fuzzy.FuzzyChoice(["ddb8d772-c0a4-42ac-9bff-fe4409495988"])
 
+
 class PDFParsingStrategyTypeFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = ParsingStrategyType
         sqlalchemy_session = pdf_session
+        sqlalchemy_session_persistence = "commit"
+
     id = fuzzy.FuzzyChoice(["9720cb1c-4461-40e4-b800-38dfdfd0061b"])
     name = fuzzy.FuzzyChoice(["unstructured"])
